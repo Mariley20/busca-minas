f95fd9c35f81588b351e21e459468f08b15c1905
Mariley20 <mariley2018@gmail.com>
2017-09-19 07:22:42 -0500

    2 click sobre bomba solucionado

--- javascript/main.js
+++ javascript/main.js
@@ -33,8 +33,8 @@ const buscaMinas = {
         });
         let bandera = true;
         while (bandera) {
-        let nroAleatorio = [];
-        let posicion = [];
+            let nroAleatorio = [];
+            let posicion = [];
             for (let i = 0; i < 11; i++) {
                 let num = Math.floor((Math.random() * 8));
                 nroAleatorio.push(num);
@@ -52,47 +52,50 @@ const buscaMinas = {
         }
         console.log(buscaMinas.posicionBombas)
     },
-    mostrarBombaHTML:(event) =>{
+    mostrarBombaHTML: (event) => {
         let indiceBomba = event.target.id;
-        let hayBomba = buscaMinas.buscarBomba(indiceBomba) 
-        console.log(hayBomba);
-        if(hayBomba){
-            console.log(hayBomba+".."+indiceBomba);
-            console.log($("td #3-4"))
-            $('td#'+indiceBomba).append(`<i class="fa fa-bomb" aria-hidden="true"></i>`);
-        }else{
-            let nroBombas = buscaMinas.buscarBombaAlrededor(indiceBomba);
+        //console.log(indiceBomba)
+        let hayBomba = buscaMinas.buscarBomba(indiceBomba)
+        if (indiceBomba != "") {
+            if (hayBomba) {
+                $('td#' + indiceBomba).append(`<i class="fa fa-bomb fa-lg" aria-hidden="true"></i>`);
+            } else {
+                let nroBombas = buscaMinas.buscarBombaAlrededor(indiceBomba);
+            }
         }
     },
     buscarBomba: (indiceBomba) => {
         let existe = undefined;
-        let x = buscaMinas.posicionBombas.filter((value) =>{
+        let x = buscaMinas.posicionBombas.filter((value) => {
             return (value == indiceBomba);
         });
-        (x.length)? existe = true : existe = false;
+        (x.length) ? existe = true: existe = false;
         return existe;
     },
     buscarBombaAlrededor: (indiceBomba) => {
         let posicionesAlrededor = [
-            [-1, -1], [-1, 0], [-1, 1],  //arriba
-            [0, -1], [0, 1],  //centro
-            [1, -1], [1, 0], [1, 1] //abajo
+            [-1, -1], // arriba izquierda
+            [-1, 0],  // arriba dentro
+            [-1, 1],  //arriba derecha
+            [0, -1],  //izquierda
+            [0, 1],   //derecha
+            [1, -1],  //abajo Inquierda
+            [1, 0],   //abajo centro
+            [1, 1]    //abajo derecha
         ];
         let indiceBombaArray = indiceBomba.split('-');
         let actualX = parseInt(indiceBombaArray[0]);
         let actualY = parseInt(indiceBombaArray[1]);
         let nroBombas = 0;
-        //console.log(indiceBombaArray);
-        console.log(posicionesAlrededor[1][0])
-        for(let i = 0; i < posicionesAlrededor.length; i++){
+        for (let i = 0; i < posicionesAlrededor.length; i++) {
             let posicionX = (actualX + posicionesAlrededor[i][0]);
             let posicionY = (actualY + posicionesAlrededor[i][1]);
-            let comprobarBomba = buscaMinas.buscarBomba(posicionX+"-"+posicionY);
-            if(comprobarBomba){
+            let comprobarBomba = buscaMinas.buscarBomba(posicionX + "-" + posicionY);
+            if (comprobarBomba) {
                 nroBombas += 1;
-            }  
+            }
         }
-        $('td#'+indiceBomba).html(nroBombas+"");
+        $('td#' + indiceBomba).html(nroBombas + "");
     }
 }
 $(document).ready(buscaMinas.inicio)
