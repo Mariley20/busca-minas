53ea42b3cde034224871310e87bb6cf94a4ad124
Mariley20 <mariley2018@gmail.com>
2017-09-19 03:14:28 -0500

    tablero realizado

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,23 @@
 'use strict'
+//mover(0, 1); // derecha
+//mover(0, -1);  //izquierda
+//mover(1, 0);//abajo
+//mover(-1, 0); //moverse arriba
+
 const buscaMinas = {
     inicio: () => {
         buscaMinas.tableroHTML();
     },
     tableroHTML: () => {
-
+        for(let i = 0; i < 8; i++){
+            $('#tablero').append(`<tr id='fila${i}'>`);
+            for(let j = 0; j < 8; j++){
+                $('#fila'+i).append(`<td id='${i},${j}'>${i},${j}</td>`);
+            }
+        }
+    },
+    ubicacionBombas: () =>{
+        
     }
-
 }
 $(document).ready(buscaMinas.inicio)
